306a814bebda8bd17213e346f66da98c669152ec
Kh4wf0x <mohamederraiss@icloud.com>
2026-06-06 03:33:05 +0200

    .

--- src/parser/map_parser.py
+++ src/parser/map_parser.py
@@ -1,4 +1,16 @@
+from typing import List
+
+class LineValidator:
+    def __init__(self, name_file: str) -> None:
+        self.name_file = name_file
 
 class Parsing:
-    def parser_args(nam_file: str) -> None:
-        print(nam_file)
+    def __init__(self, name_file: str) -> None:
+        self.start_hub: Start_hub = None
+        self.hub: List[Hub]= []
+        self.end_hub: End_hub = None
+        self.connection: List = []
+        self.git_line: Git_line = Git_line(name_file)
+
+    def parser_args(self) -> None:
+        pass
