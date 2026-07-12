a92d79950373b44ee15d9968099370d702133e8c
SANTOOS00 <mohamederraiss@icloud.com>
2026-07-12 07:33:57 +0100

    .

--- network.py
+++ network.py
@@ -17,6 +17,95 @@ class NetworkTopologyBuilder:
         self.network_link = defaultdict(list)
 
     def build(self, network: FlightNetwork) -> None:
-        connections = network.get_connections()
-        for conn in connections:
-            pass
+        pass
+
+
+adj_list = {
+    "name_zone": "zone_1",
+    "data": [
+        {
+            "to": "zone_2",
+            "meta": {
+                "max_capacity": 2
+            }
+        }
+    ],
+    "zone_2": {
+        "color": "red",
+    },
+    "data": [
+        {
+            "to": "zone_1",
+            "meta": {
+                "max_capacity": 2
+            }
+        },
+        {
+            "to": "zonr_3",
+            "meta": {
+                "max_capacity": 3
+            }
+        }
+    ]
+}
+
+
+adj_list = {
+    "start": {
+        "meta": {
+            "color": "green",
+            "max_drones": 12,
+            "zone": "normal"
+        },
+        "edges": [{
+            "to": "waypoint1",
+            "max_link_capacity": 1
+        }
+        ]
+    },
+    "waypoint1": {
+        "meta": {
+            "color": "blue",
+            "max_drones": 1,
+            "zone": "normal"
+        },
+        "edges": [{
+            "to": "waypoint2",
+            "max_link_capacity": 1
+         },
+         {
+            "to": "waypoint2",
+            "max_link_capacity": 1
+         },
+
+        ]
+    },
+    "waypoint2": {
+        "meta": {
+            "color": "blue",
+            "max drones": 1
+        },
+        "edges": [
+            {
+                "to": "waypoint1",
+                "max_link_capacity": 1
+            },
+            {
+                "to": "goal",
+                "max_link_capcity": 1
+            }
+        ]
+    },
+    "goal": {
+        "meta": {
+            "color": "red",
+            "max_drones": 1
+        },
+        "edges": [
+            {
+                "to": "waypoint1",
+                "max_link_capackity": 1
+            }
+        ]
+    }
+}
