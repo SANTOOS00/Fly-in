d4c880bba03f01c1122d91ffd73a56ab02542515
SANTOOS00 <mohamederraiss@icloud.com>
2026-07-18 13:20:10 -0400

    version 0.0.2

--- custom_error.py
+++ custom_error.py
@@ -1,116 +1,144 @@
-from enum import Enum
+# from enum import Enum
 
 
-class ErrorSeverity(Enum):
-    Warning = "Warning"
-    Error = "Error"
+# class ErrorSeverity(Enum):
+#     Warning = "Warning"
+#     Error = "Error"
 
 
-class ErrorLocation(Enum):
-    ZONE = "Zone name is not valid"
-    X_AXIS = "X coordinate is not valid"
-    Y_AXIS = "Y coordinate is not valid"
+# class ErrorLocation(Enum):
+#     ZONE = "Zone name is not valid"
+#     X_AXIS = "X coordinate is not valid"
+#     Y_AXIS = "Y coordinate is not valid"
 
 
-class BaseError(Exception):
-    """
-    #|------------------------------------------------------------------|
-    #|            ----- custom errors in project ------                 |
-    #|------------------------------------------------------------------|
-    """
+# class BaseError(Exception):
+#     """
+#     #|------------------------------------------------------------------|
+#     #|            ----- custom errors in project ------                 |
+#     #|------------------------------------------------------------------|
+#     """
 
-    def __init__(self, message: str, line_number: int | None = None,
-                 severity: ErrorSeverity | None = None) -> None:
+#     def __init__(self, message: str, line_number: int | None = None,
+#                  severity: ErrorSeverity | None = None) -> None:
+#         super().__init__(message)
+#         self.message = message
+#         self.line_number = line_number
+#         self.severity = severity
+
+#     def __str__(self) -> str:
+#         return (
+#             f"[{self.severity.value}] line {self.line_number}: "
+#             f"{self.message}")
+
+
+# class PathError(BaseError):
+#     def __init__(self, message: str, severity: ErrorSeverity | None = None
+#                  ) -> None:
+#         super().__init__(message, severity=severity)
+
+#     def __str__(self) -> str:
+#         return f"[{self.severity.value}]: {self.message}"
+
+
+# class HubError(BaseError):
+#     def __init__(self, message: str, line_number: int, severity: ErrorSeverity
+#                  ) -> None:
+#         super().__init__(message, line_number, severity)
+
+#     def __str__(self) -> str:
+#         return (
+#                 f"\n[{self.severity.value}] Line {self.line_number}\n"
+#                 f"  ➜ Input : {self.message}\n"
+#             )
+
+
+# class ConnectionError(BaseError):
+#     def __init__(self, message: str, line_number: int,
+#                  severity: ErrorSeverity) -> None:
+#         super().__init__(message, line_number, severity)
+
+#     def __str__(self):
+#         return (
+#                 f"\n[{self.severity.value}] Line {self.line_number}\n"
+#                 f"  ➜ Input : {self.message}\n"
+#         )
+
+
+# class UtilsError(BaseError):
+#     def __init__(self, message: str, line_number: int | None = None,
+#                  severity: ErrorSeverity | None = None) -> None:
+#         super().__init__(message, line_number, severity)
+
+#     def __str___(self):
+#         return (
+#                 f"\n[{self.severity.value}] Line {self.line_number}\n"
+#                 f"  ➜ Input : {self.message}\n"
+#             )
+
+
+# class ZoneWithCoordsParserError(BaseError):
+#     def __init__(self,
+#                  message: str,
+#                  line_number: int | None = None,
+#                  severity: ErrorSeverity | None = None,
+#                  location: ErrorLocation | None = None) -> None:
+#         super().__init__(message, line_number, severity)
+#         self.location = location
+
+#     def __str__(self) -> None:
+#         if self.location == ErrorLocation.ZONE:
+#             return (
+#                 f"\n[{self.severity.value}] Line {self.line_number}"
+#                 f" at {self.location.value}:\n"
+#                 f"  ➜ Input : {self.message}\n"
+#                 f"  ⚠  Fix: {self.severity.value} ⚠  Fix: Error: Invalid zone "
+#                 "format. The zone name may contain any characters "
+#                 "except spaces and hyphens ('-')..\n"
+#             )
+#         else:
+#             return (
+#                 f"\n[{self.severity.value}] Line {self.line_number}"
+#                 f" at {self.location.value}:\n"
+#                 f"  ➜ Input : {self.message}\n"
+#                 f"  ⚠  Fix: {self.severity.value} coordinates must be integers"
+#                 " or negative numbers only.\n"
+#             )
+
+
+# class MetaDataParserError(BaseError):
+#     def __init__(self, message: str, line_number: int, location: ErrorLocation
+#                  ) -> None:
+#         super().__init__(message, line_number, location)
+
+#     def __str__(self) -> str:
+#         return (f"\n[{self.severity.value}] Line {self.line_number}\n"
+#                 f"  ➜ Input : {self.message}\n")
+from typing import Dict, Callable, Any
+
+class   FlyinError(Exception):
+    def __init__(self, message: str, **context) -> None:
         super().__init__(message)
-        self.message = message
-        self.line_number = line_number
-        self.severity = severity
+        self.context: Dict[str, str] = context
+    
+    def __add__(self, other: Dict[str, str]) -> 'FlyinError':
+        self.context.update(other)
+        return self
+    
+    def report(self) -> str:
+        print(self.context)     
+        print(self)
 
-    def __str__(self) -> str:
-        return (
-            f"[{self.severity.value}] line {self.line_number}: "
-            f"{self.message}")
-
-
-class PathError(BaseError):
-    def __init__(self, message: str, severity: ErrorSeverity | None = None
-                 ) -> None:
-        super().__init__(message, severity=severity)
-
-    def __str__(self) -> str:
-        return f"[{self.severity.value}]: {self.message}"
-
-
-class HubError(BaseError):
-    def __init__(self, message: str, line_number: int, severity: ErrorSeverity
-                 ) -> None:
-        super().__init__(message, line_number, severity)
-
-    def __str__(self) -> str:
-        return (
-                f"\n[{self.severity.value}] Line {self.line_number}\n"
-                f"  ➜ Input : {self.message}\n"
-            )
+    def check_error(type_error: str) -> Callable:
+        def decorator(func: Callable) -> Callable:
+            def wrapper(*args, **kwargs) -> Any:
+                try:
+                    func(*args, **kwargs)
+                except FlyinError as error:
+                    raise FlyinError(str(error),
+                                    type_error=type_error,
+                                    ) + error.context
+            return wrapper
+        return decorator
 
 
-class ConnectionError(BaseError):
-    def __init__(self, message: str, line_number: int,
-                 severity: ErrorSeverity) -> None:
-        super().__init__(message, line_number, severity)
-
-    def __str__(self):
-        return (
-                f"\n[{self.severity.value}] Line {self.line_number}\n"
-                f"  ➜ Input : {self.message}\n"
-        )
-
-
-class UtilsError(BaseError):
-    def __init__(self, message: str, line_number: int | None = None,
-                 severity: ErrorSeverity | None = None) -> None:
-        super().__init__(message, line_number, severity)
-
-    def __str___(self):
-        return (
-                f"\n[{self.severity.value}] Line {self.line_number}\n"
-                f"  ➜ Input : {self.message}\n"
-            )
-
-
-class ZoneWithCoordsParserError(BaseError):
-    def __init__(self,
-                 message: str,
-                 line_number: int | None = None,
-                 severity: ErrorSeverity | None = None,
-                 location: ErrorLocation | None = None) -> None:
-        super().__init__(message, line_number, severity)
-        self.location = location
-
-    def __str__(self) -> None:
-        if self.location == ErrorLocation.ZONE:
-            return (
-                f"\n[{self.severity.value}] Line {self.line_number}"
-                f" at {self.location.value}:\n"
-                f"  ➜ Input : {self.message}\n"
-                f"  ⚠  Fix: {self.severity.value} ⚠  Fix: Error: Invalid zone "
-                "format. The zone name may contain any characters "
-                "except spaces and hyphens ('-')..\n"
-            )
-        else:
-            return (
-                f"\n[{self.severity.value}] Line {self.line_number}"
-                f" at {self.location.value}:\n"
-                f"  ➜ Input : {self.message}\n"
-                f"  ⚠  Fix: {self.severity.value} coordinates must be integers"
-                " or negative numbers only.\n"
-            )
-
-
-class MetaDataParserError(BaseError):
-    def __init__(self, message: str, line_number: int, location: ErrorLocation
-                 ) -> None:
-        super().__init__(message, line_number, location)
-
-    def __str__(self) -> str:
-        return (f"\n[{self.severity.value}] Line {self.line_number}\n"
-                f"  ➜ Input : {self.message}\n")
--- networknode.py
+++ networknode.py
@@ -1,71 +1,110 @@
-from typing import Protocol, List, Dict, Any
-from custom_error import ErrorSeverity, HubError
+from dataclasses import dataclass
+from enum import Enum
+from custom_error import FlyinError
+
+@dataclass(frozen=True)
+class Hub:
+    name: str
+    type: "Hub.Type" = None
+    zone: "Hub.Zone" = None
+    max_drones: int = 1
+    color: str = '#FFFFFF'
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    class Color(Enum):
+        BLACK = "black"
+        RED = "red"
+        GREEN = "green"
+        YELLOW = "yellow"
+        BLUE = "blue"
+        MAGENTA = "magenta"
+        CYAN = "cyan"
+        WHITE = "white"
+        ORANGE = "orange"
+        PURPLE = "purple"
+        BROWN = "brown"
+        LIME = "lime"
+        GOLD = "gold"
+        MAROON = "maroon"
+        DARKRED = "darkred"
+        VIOLET = "violet"
+        CRIMSON = "crimson"
+        RAINBOW = "rainbow"
+
+        @classmethod
+        def get_hex(cls, type_color: str) -> str:
+            try:
+                c = cls(type_color.lower())
+                return COLOR_HEX[c]
+            except Exception:
+                return '#FFFFFF'
+
+    class Type(Enum):
+        NORMAL_HUB = "Normal_hub"
+        START_HUB = "Start_hub"
+        END_HUB = "End_hub"
+
+        @classmethod
+        def get_type_hube(cls, type_str: str) -> "Hub.Type":
+            match type_str.upper():
+                case "START_HUB":
+                    return cls.START_HUB
+                case "END_HUB":
+                    return cls.END_HUB
+                case "HUB":
+                    return cls.NORMAL_HUB
+                case _:
+                    raise FlyinError("Invalid hub type found during parsing.", type_error="invalid_hub_type")
+
+    class Zone(Enum):
+        PRIORITY = 1
+        NORMAL = 1
+        RESTRICTED = 2
+        BLOCKED = float('inf')
+
+        @classmethod
+        def get_type_zone(cls, type_zone: str) -> "Hub.Zone":
+            match type_zone.upper():
+                case "NORMAL":
+                    return cls.NORMAL
+                case "BLOCKED":
+                    return cls.BLOCKED
+                case "PRIORITY":
+                    return cls.PRIORITY
+                case "RESTRICTED":
+                    return cls.RESTRICTED
+                case _:
+                    raise FlyinError("Invalid zone type found during parsing.", type_error="invalid_zone_type")
+
+Hub.type = Hub.Type.NORMAL_HUB
+Hub.zone = Hub.Zone.NORMAL
+
+COLOR_HEX = {
+    Hub.Color.BLACK: "#000000",
+    Hub.Color.RED: "#FF0000",
+    Hub.Color.GREEN: "#00FF00",
+    Hub.Color.YELLOW: "#FFFF00",
+    Hub.Color.BLUE: "#0000FF",
+    Hub.Color.MAGENTA: "#FF00FF",
+    Hub.Color.CYAN: "#00FFFF",
+    Hub.Color.WHITE: "#FFFFFF",
+    Hub.Color.ORANGE: "#FFA500",
+    Hub.Color.PURPLE: "#800080",
+    Hub.Color.BROWN: "#A52A2A",
+    Hub.Color.LIME: "#00FF00",
+    Hub.Color.GOLD: "#FFD700",
+    Hub.Color.MAROON: "#800000",
+    Hub.Color.DARKRED: "#8B0000",
+    Hub.Color.VIOLET: "#EE82EE",
+    Hub.Color.CRIMSON: "#DC143C",
+    Hub.Color.RAINBOW: "#FF69B4",
+}
 
 
-class NetworkNode(Protocol):
-    ...
 
-
-class Drones(NetworkNode):
-    def __init__(self, number_drones: int) -> None:
-        self.drones = number_drones
-     
-
-class End_hub(NetworkNode):
-    _number_line_start = None
-    _instance: bool = False
-
-    def __init__(self, name: str, y: int, x: int,
-                 meta: Dict[str, Any] | None = None,
-                 line_number: int | None = None) -> "Start_hub":
-        if End_hub._instance:
-            raise HubError(
-                "Duplicate Start hub at lines "
-                f"{Start_hub._instance._number_line_start} "
-                f"and {line_number}. Only one is allowed.",
-                line_number,
-                ErrorSeverity.Error)
-        End_hub._number_line_start = line_number
-        End_hub._instance = True
-        self.name = name
-        self.y = y
-        self.x = x
-        self.meta = meta
-
-
-class Start_hub(NetworkNode):
-    _number_line_start = None
-    _instance: bool = False
-
-    def __init__(self, name: str, y: int, x: int,
-                 meta: Dict[str, Any] | None = None,
-                 line_number: int | None = None) -> "Start_hub":
-        if Start_hub._instance:
-            raise HubError(
-                "Duplicate Start hub at lines "
-                f"{Start_hub._instance._number_line_start} "
-                f"and {line_number}. Only one is allowed.",
-                line_number,
-                ErrorSeverity.Error)
-        Start_hub._number_line_start = line_number
-        Start_hub._instance = True
-        self.name = name
-        self.y = y
-        self.x = x
-        self.meta = meta
-
-
-class Hub(NetworkNode):
-    def __init__(self, name: str, y: int, x: int,
-                 meta: Dict[str, Any] | None = None
-                 ) -> None:
-        self.name = name
-        self.x = x
-        self.y = y
-        self.meta: Dict[str: Any] | None = meta
-
-
-class Connection:
-    def __init__(self, connection: set, meta) -> None:
-        self.connection = connection
-        self.meta = meta
+@dataclass
+class Edge:
+    source: Hub
+    destintion: Hub
