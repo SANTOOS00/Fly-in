23e98ecb46a0b9b02bdd0adf700542a7d64bd6ff
SANTOOS00 <mohamederraiss@icloud.com>
2026-07-21 12:08:24 -0400

    fix: parser config and parser meta data

--- graph.py
+++ graph.py
@@ -49,18 +49,30 @@ class Graph:
     
     @FlyinError.check_error(type_error='testsss')
     def add_egde(self, edge: Edge) -> None:
-        self._init_edge_for_hub(edge)
-        print(edge)
+        self._init_edge_for_hubs(edge)
         if {edge.source, edge.destintion} in [{edg.source, edg.destintion}
                                               for edg in self.edges]:
             raise FlyinError("testsssssssssssss",
                              line_number=FlyinError.get_number_line())
-        self.edges.append(edge)
-    
+        self.edges.append(edge)    
 
-    def _init_edge_for_hub(self, edge: Edge) -> None:
-        edge.source = self.hubs[edge.source]
-        edge.destintion = self.hubs[edge.destintion]
+    def _init_edge_for_hubs(self, edge: Edge) -> None:
+        self._set_hub_source(edge)
+        self._set_hub_destintion(edge)
+
+    def _set_hub_source(self, edge: Edge) -> None:
+        if self.hubs.get(edge.source):
+            edge.source = self.hubs[edge.source]
+        else:
+            raise FlyinError("",
+                             line_number=FlyinError.get_number_line())
+
+    def _set_hub_destintion(self, edge: Edge) -> None:
+        if self.hubs.get(edge.destintion):
+            edge.destintion = self.hubs[edge.destintion]
+        else:
+            raise FlyinError("",
+                             line_number=FlyinError.get_number_line())
 
     def _get_source_hub(self, name_zone: str) -> Hub:
         return self.get_hub(name_zone)
