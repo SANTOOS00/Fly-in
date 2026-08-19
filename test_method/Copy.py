13b53522f286220a1cb16996849753aaba295f14
SANTOOS00 <mohamederraiss@icloud.com>
2026-08-19 03:46:19 +0100

    fix: finihsd in simulation :)

--- test_method/Copy.py
+++
@@ -1,6 +0,0 @@
-def starts_a(w):
-    return w.startswith("a")
-
-li = ["apple", "banana", "avocado", "cherry", "apricot"]
-res = filter(lambda x: x.startswith('a'), li)
-print(list(res))
--- test_method/test_color.py
+++
@@ -1,9 +0,0 @@
-from rich import print
-import webcolors
-
-print(webcolors.name_to_hex("brown"))
-sss = str(webcolors.name_to_hex("brown")).upper()
-print(sss)
-print(f"[{sss}]ss[/{sss}]")
-print("[green]ss[/green]")
-print("[bold blu]ss[/bold blu]")
